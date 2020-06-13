82a66dc6363593f4f7cd4e9f5b11336cd700a353
Harold Oudshoorn <harold.oudshoorn@gmail.com>
2020-06-13 17:29:42 +0200

    Fixed wrong X and Y value

--- octoprint_meshadjust/static/js/meshadjust.js
+++ octoprint_meshadjust/static/js/meshadjust.js
@@ -4,68 +4,73 @@
  * Author: Harold Oudshoorn
  * License: AGPLv3
  */
-$(function() {
+$(function () {
   function MeshAdjustViewModel(parameters) {
     var self = this;
 
-    self.settings = parameters[0]
-    self.loginState = parameters[1]
+    self.currentX = 0;
+    self.currentY = 0;
 
-    self.bedWidth = ko.observable("200.00")
-    self.bedDepth = ko.observable("200.00")
-    self.edgeOffset = ko.observable("15.00")
-    self.feedrate = ko.observable("1000")
+    self.code = "";
+
+    self.settings = parameters[0];
+    self.loginState = parameters[1];
+
+    self.bedWidth = ko.observable("200.00");
+    self.bedDepth = ko.observable("200.00");
+    self.edgeOffset = ko.observable("15.00");
+    self.feedrate = ko.observable("1000");
     self.isOperational = ko.observable(undefined);
     self.isPrinting = ko.observable(undefined);
 
-    self.testPrint = function(wMult, dMult) {
-      wEffective = self.bedWidth() - 2* self.edgeOffset()
-      dEffective = self.bedDepth() - 2* self.edgeOffset()
-      xPos = 1.0*self.edgeOffset() + (wMult*wEffective);
-      yPos = 1.0*self.edgeOffset() + (dMult*dEffective);
-      code = "G0";
-      code += " X" + xPos;
-      code += " Y" + yPos;
-      code += " F" + self.feedrate();
-      //OctoPrint.control.sendGcode(code);
-      console.log("TouchTest: Sending command \"" + code +"\"");
+    self.adjustMeshPoint = function (zAdjust) {
+      self.code = "G1 Z" + zAdjust;
+      OctoPrint.control.sendGcode("G91");
+      OctoPrint.control.sendGcode(self.code);
+      OctoPrint.control.sendGcode("G90");
+      console.log("MeshAdjust: Sending command \"" + self.code + "\"");
+
+      self.code = "M421 I" + self.currentX + " J" + self.currentY + " Q" + zAdjust;
+      OctoPrint.control.sendGcode(self.code);
+      console.log("MeshAdjust: Sending command \"" + self.code + "\"");
     }
 
-    self.adjustMesh = function(xIndex, yIndex, value) {
-      code = "M421";
-      code += " I" + xIndex;
-      code += " J" + yIndex;
-      code += " Q" + value;
-      OctoPrint.control.sendGcode(code);
-      console.log("MeshAdjust: Sending command \"" + code +"\"");
+    self.gotoMeshPoint = function (xIndex, yIndex) {
+      self.currentX = xIndex;
+      self.currentY = yIndex;
+      self.code = "G42 I" + xIndex + " J" + yIndex + " F4000";
+      OctoPrint.control.sendGcode("G1 Z5 F1000");
+      OctoPrint.control.sendGcode(self.code);
+      OctoPrint.control.sendGcode("G1 Z0.10 F1000");
+      console.log("MeshAdjust: Sending command \"" + self.code + "\"");
     }
 
-    self.onBeforeBinding = function() {
+    self.onBeforeBinding = function () {
       self.feedrate(self.settings.settings.plugins.meshadjust.feedrate());
     }
 
-    self.fromCurrentData = function(data) {
+    self.fromCurrentData = function (data) {
       self._processStateData(data.state);
     };
 
-    self.fromHistoryData = function(data) {
+    self.fromHistoryData = function (data) {
       self._processStateData(data.state);
     };
 
-    self._processStateData = function(data) {
+    self._processStateData = function (data) {
       self.isOperational(data.flags.operational);
       self.isPrinting(data.flags.printing);
     };
 
-    self.movementEnabled = function() {
-      return (self.isOperational() && self.loginState.isUser() && ! self.isPrinting());
+    self.movementEnabled = function () {
+      return (self.isOperational() && self.loginState.isUser() && !self.isPrinting());
     }
   }
 
   // view model class, parameters for constructor, container to bind to
   OCTOPRINT_VIEWMODELS.push([
     MeshAdjustViewModel,
-    [ "settingsViewModel", "loginStateViewModel" ],
-    [ "#sidebar_plugin_meshadjust" ]
+    ["settingsViewModel", "loginStateViewModel"],
+    ["#sidebar_plugin_meshadjust"]
   ]);
 });
